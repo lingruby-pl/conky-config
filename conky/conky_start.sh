bce6bfbf65f3f21922e12ac89f44bff9f2ac24cd
Jacek Bienias <sp7ezd@gmail.com>
2020-02-09 20:56:56 +0100

    Add files via upload

--- conky/conky_start.sh
+++ conky/conky_start.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 killall conky
+
 conky -c "/home/lingruby/.conky/conky/conky" &
 conky -c "/home/lingruby/.conky/conky/conky_txt" &
 conky -c "/home/lingruby/.conky/conky/conky_clock" &
+
 exit 0
